f6dd933efd83747ba0cecebc5b7c1068dc60b45b
Cyb10101 <cyb10101@gmail.com>
2020-09-26 15:05:18 +0200

    Fix permissions in scripts

--- config/bin/entrypoint.d/90-cron.sh
+++ config/bin/entrypoint.d/90-cron.sh
@@ -5,8 +5,11 @@ CRONTAB_FILE=/root/scripts/crontab.txt
 # Add crontab file
 if [[ ! -f ${CRONTAB_FILE} ]]; then
   cp /opt/docker/crontab.txt ${CRONTAB_FILE}
-  chmod 666 ${CRONTAB_FILE}
 fi
+
+# Fix permissions
+find /root/scripts -type f ! -iname '*.txt' -exec chmod 777 {} \;
+chmod 666 ${CRONTAB_FILE}
 
 # Refresh crontab
 crontab ${CRONTAB_FILE}
